363c81bee62dffd8cad9b559aa6a2a6f081c6570
madyanes <madyanes.dev@gmail.com>
2023-08-06 00:48:44 +0700

    create initial state of theme and toggle function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState((prevState) => {
+          return {
+            theme: prevState.theme === 'light' ? 'dark' : 'light'
+          }
+        })
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <p>Hello, World</p>
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <p>Hello, World</p>
+        </div>
+      </ThemeProvider>
     );
   }
 }
